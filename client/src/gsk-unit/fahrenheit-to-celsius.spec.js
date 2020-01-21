3d7b86fac0aff7f506cd3345326a8383cd0c5aee
Santosh <gskumar1010@gmail.com>
2020-01-21 14:51:35 -0500

    update

--- client/src/gsk-unit/fahrenheit-to-celsius.spec.js
+++ client/src/gsk-unit/fahrenheit-to-celsius.spec.js
@@ -1,5 +1,21 @@
+const fahrenheitToCelsius = (degreesFahrenheit)=> {
+    if (typeof degreesFahrenheit != 'number'){
+        throw new Error ('the value is not a number');
+    }
+
+    return (degreesFahrenheit -32) * 5/9;
+}    
+
 describe('fahrenheit-to-celsius', () => {
     test('canary verifies test tools', () => {
         expect(true).toEqual(true);
     });
+    test('32 degrees fahrenheit is 0 degress celsius', () => {
+        expect(fahrenheitToCelsius(32)).toEqual(0);
+    });
+
+    test('212 degrees fahrenheit is 100 degress celsius', () => {
+        expect(fahrenheitToCelsius(212)).toEqual(100);
+    });
+    
 });
