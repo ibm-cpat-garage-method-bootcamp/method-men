97ebb41c32dbbe35ca12ad2dfe100bc7c6247270
KEATON TATOOLES <keaton.tatooles@ibm.com@keatons-mbp.raleigh.ibm.com>
2020-01-21 14:50:44 -0500

    add expects to fahrenheit-to-celsius test

--- client/src/kwt-unit/fahrenheit-to-celsius.spec.js
+++ client/src/kwt-unit/fahrenheit-to-celsius.spec.js
@@ -1,5 +1,27 @@
+const fahrenheitToCelsius = (degreesFahrenheit) => {
+    if (typeof degreesFahrenheit !== 'number') {
+        throw new Error('the value is not a number');
+    }
+
+    return (degreesFahrenheit - 32) * 5 / 9;
+}
+
 describe('fahrenheit-to-celsius', () => {
     test('canary verifies test tools', () => {
-        expect(true).toEqual(false);
+        expect(true).toEqual(true);
+    });
+
+    test('32 degrees fahrenheit is 0 degrees celsius', () => {
+        expect(fahrenheitToCelsius(32)).toEqual(0);
+    })
+
+    test('212 degrees fahrenheit is 100 degrees celsius', () => {
+        expect(fahrenheitToCelsius(212)).toEqual(100);
+    });
+
+    test('when value is not a number then throw an error', () => {
+        expect(() => {
+            fahrenheitToCelsius('test');
+        }).toThrow(new Error('the value is not a number'));
     });
 });
