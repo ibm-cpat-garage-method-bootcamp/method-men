f684fc5c59d81ebf3c3532897a2ff3a64fa25478
Santosh <gskumar1010@gmail.com>
2020-01-21 14:55:16 -0500

    update

--- client/src/gsk-unit/fahrenheit-to-celsius.spec.js
+++ client/src/gsk-unit/fahrenheit-to-celsius.spec.js
@@ -17,5 +17,9 @@ describe('fahrenheit-to-celsius', () => {
     test('212 degrees fahrenheit is 100 degress celsius', () => {
         expect(fahrenheitToCelsius(212)).toEqual(100);
     });
-    
+    test('when value is not a number then throw an error', () => {
+        expect(() => {
+            fahrenheitToCelsius('test');
+        }).toThrow(new Error('the value is not a number'));
+    });
 });
