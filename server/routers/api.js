c83433777e99634c190c99c707067faf22d0b264
KEATON TATOOLES <keaton.tatooles@ibm.com@keatons-mbp.raleigh.ibm.com>
2020-01-22 13:41:16 -0500

    Change request handler to middleware function

--- server/routers/api.js
+++ server/routers/api.js
@@ -4,13 +4,25 @@ const fs = require('fs');
 
 const apiHost = process.env.API_HOST || 'localhost:3001';
 
+// module.exports = function(app){
+//   app.use('/api/data', (req, res) => {
+//     fs.readFile('./server/data/db.json', 'utf8', (err, data) => {
+//       console.log(data);
+//     })
+//     console.log('hit the thing');
+//     console.log(process.cwd());
+//     res.send('you got the data!!!');
+//   });
+// };
+
 module.exports = function(app){
-  app.use('/api/data', (req, res) => {
-    fs.readFile('./server/data/db.json', 'utf8', (err, data) => {
-      console.log(data);
-    })
-    console.log('hit the thing');
-    console.log(process.cwd());
-    res.send('you got the data!!!');
-  });
+  app.use('/api/data', dataRequestHandler);
 };
+
+const dataRequestHandler = (req, res) => {
+  fs.readFile('./server/data/db.json', 'utf8', (err, data) => {
+    console.log(data);
+  })
+  console.log('hit the thing');
+  res.send('you got the data!!!');
+}
